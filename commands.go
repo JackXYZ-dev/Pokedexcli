8f8abbd64362d7c51ed68d8e387ddd98a7a8fa18
jackXYZ-dev <jackrobertsxyz@pm.me>
2024-10-03 06:37:21 +0100

    REPL: help and exit commands

---
+++ commands.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	fmt.Println("help: Displays a help message")
+	fmt.Println("exit: Exit the Pokedex")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Goodbye!")
+	os.Exit(0)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Printn("Hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+	fmt.Print("Pokedex > ")
+
+	for scanner.Scan() {
+		input := strings.ToLower(scanner.Text())
+
+		if command, exists := commands[input]; exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			fmt.Println("Unknown command. Type 'help' for a list of commands.")
+		}
+
+		fmt.Print("Pokedex > ")
+	}
 }
